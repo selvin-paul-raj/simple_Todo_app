79f62bbf78583dbba093e5156222733bdee489a0
sprhackz <sprhackz003@gmail.com>
2023-11-15 20:50:02 +0530

    Simple Todo App using Html,css,js

---
+++ script.js
@@ -0,0 +1,41 @@
+let button = document.getElementById("add");
+let todoList = document.getElementById("todoList");
+let input = document.getElementById("input");
+//local storage,cookies
+let todos = [];
+window.onload = () => {
+  todos = JSON.parse(localStorage.getItem("todos")) || [];
+  todos.forEach((todo) => addtodo(todo));
+};
+
+button.addEventListener("click", () => {
+  todos.push(input.value);
+  localStorage.setItem("todos", JSON.stringify(todos));
+  addtodo(input.value);
+  input.value = "";
+});
+
+function addtodo(todo) {
+  let para = document.createElement("p");
+  para.innerText = todo;
+  para.style.textAlign = "center";
+  todoList.appendChild(para);
+
+  para.addEventListener("click", () => {
+    para.style.textDecoration = "line-through red";
+    para.style.textDecorationColor="red";
+    remove(todo);
+  });
+  para.addEventListener("dblclick", () => {
+    todoList.removeChild(para);
+    remove(todo);
+  });
+}
+
+function remove(todo) {
+  let index = todos.indexOf(todo);
+  if (index > -1) {
+    todos.splice(index, 1);
+  }
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
